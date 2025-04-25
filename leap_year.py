5e97c83312e3187ef9007819f89da4b7a2e24780
Jeremiah <sentomerojeremy@gmail.com>
2025-04-25 08:49:35 +0300

    new commit

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+def is_leap(year) -> bool:
+    leap = False
+    year = int(input("Enter a year: "))
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+    
